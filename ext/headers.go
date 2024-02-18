c05e4e54883b73cd8bf0afb480d11551f64be17c
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2024-02-18 10:25:59 -0600

    update comments

--- ext/headers.go
+++ ext/headers.go
@@ -2,8 +2,8 @@ package htmx_ext
 
 // htmx custom extension headers
 const (
-	// HxTitle is associated with a custom htmx extension, "title-header", that intercepts this response header and updates the document's title to the new value when present.
+	// HxTitle is a response header associated with a custom htmx extension, "title-header", that intercepts this response header and updates the document's title to the new value when present.
 	HxTitle string = "Hx-Title"
-	// HxIcon is associated with a custom htmx extension, "icon-header", that intercepts this response header and updates the document's icon link element when present.
+	// HxIcon is a response header associated with a custom htmx extension, "icon-header", that intercepts this response header and updates the document's icon link element when present.
 	HxIcon string = "Hx-Icon"
 )
